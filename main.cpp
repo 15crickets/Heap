e8181e9e201b83773a0065f761ad687ab0bd2dcb
U-BSD\480143 <480143@SHS-8YBF6X3.beaverton.k12.or.us>
2024-02-26 11:48:43 -0800

    Finished

--- main.cpp
+++ main.cpp
@@ -1,72 +1,115 @@
+/*
+Author: Vikram Vasudevan
+Date: 2/26/2024
+Description: This program takes in integer inputs and arranges them in a max heap data structure.
+
+ */
+
 #include <iostream>
 #include <cstring>
 #include <vector>
 #include <math.h>
+#include "fstream"
 using namespace std;
 
-void add(int (&nums)[101], int &numTracker, int value);
+//function prototypes
+void add (int (&nums)[101], int &numTracker);
+void addMethod (int (&nums)[101], int &numTracker, int value);
 void remove(int (&nums)[101], int &numTracker);
 void sort(int (&nums)[101], int index);
-void display(int nums [101], int numTracker);
+void display(int nums [101], int numTracker, int count, int index);
 void deleteSort(int (&nums)[101], int numTracker, int index);
+void removeAll(int (&nums)[101], int &numTracker);
 
+//main
 int main(){
+  //initializing variables + arrays.
+  int numTracker = 0;
+  int nums[101];
+  bool stillRunning = true;
+  //while loop where program runs until quit.
+  while(stillRunning == true){
+    //inputting user choice
+    cout << "What would you like to do: ADD, REMOVE, REMOVE ALL, PRINT, or QUIT" << endl;
+    char choice [15];
+    cin.get(choice, 15);
+    cin.get();
+    if(strcmp(choice, "ADD") == 0){
+      numTracker = numTracker + 1;
+      add(nums, numTracker);
+ 
+    }
+    if(strcmp(choice, "REMOVE") == 0){
+      remove(nums, numTracker);
+    }
+    if(strcmp(choice, "PRINT") == 0){
+      display(nums, numTracker, 0, 1);
+    }
+    if(strcmp(choice, "REMOVE ALL") == 0){
+      removeAll(nums, numTracker);
+    }
+    if(strcmp(choice, "QUIT") == 0){
+      stillRunning = false;
+    }
+  }
+
+}
+//function that removes everything from the table
+void removeAll(int (&nums)[101], int &numTracker){
+  while(numTracker > 0){
+    remove(nums, numTracker);
+  }
+
+
+}
+
+//add function
+void add (int (&nums)[101], int &numTracker){
+  //determining add method
   cout << "Are you entering data by console or by file? Type c for console and f for file" << endl;
   char method;
   cin >> method;
   cin.get();
-  int numTracker = 1;
-  int nums[101];
   if(method == 'c'){
+    //determininig the number of numbers to add.
     cout << "How many numbers will you be inputting? " << endl;
     int numNums;
     cin >> numNums;
     cin.get();
-    nums[0] = 599;
+    //adding the numbers into the nums array.
     for(int i = 0; i < numNums; i++){
       int x = 0;
       cin >> x;
       cin.get();
       nums[numTracker] = x;
-      cout << "X: " << x          << endl;
-      add(nums, numTracker, nums[numTracker]);
+      addMethod(nums, numTracker, nums[numTracker]);
       
     }
     numTracker = numTracker - 1;
-    cout << nums[10] << " " << nums[11] << " " << endl;
-    display(nums, numTracker);    
+    display(nums, numTracker, 0, 1);    
   }
-  if(method == 'f'){
-    cout << "F" << endl;
+  //handling inputting by file
+  else if(method == 'f'){
+    ifstream fin("numbers.txt");
+    
+    while(fin >> nums[numTracker]){
+      sort(nums, numTracker);
+      numTracker++;
+    }
+    numTracker = numTracker - 1;
   }
-  bool stillRunning = true;
   
-  while(stillRunning == true){
-    cout << "What would you like to do: ADD, REMOVE, REMOVE ALL, PRINT" << endl;
-    char choice [15];
-    cin.get(choice, 15);
-    cin.get();
-    if(strcmp(choice, "ADD") == 0){
-      cout << "WEVIBE" << endl;
-
-    }
-    if(strcmp(choice, "REMOVE") == 0){
-      cout << "REMOVE" << endl;
-      remove(nums, numTracker);
-    }
-    if(strcmp(choice, "PRINT") == 0){
-      display(nums, numTracker);
-    }
-  }
-
 }
 
+//this function sorts the table after a new number has been added.
 void sort(int (&nums)[101], int index){
   int reducedIndex = floor(index/2);
-  cout << "REDUCEDINDEX: " << reducedIndex << endl;
+  
   if(reducedIndex == 0){
     return;
   }
+  
+  //swapping values and recalling the function.
   if(nums[index] > nums[reducedIndex]){
     int temp = nums[index];
     nums[index] = nums[reducedIndex];
@@ -76,23 +119,25 @@ void sort(int (&nums)[101], int index){
   return;
   
 }
+
+//this function sorts the table after the remove function is called.
 void deleteSort(int (&nums)[101], int numTracker, int index){
   int leftTotal = 0;
   int rightTotal = 0;
+  //calculating whether a sort needs to occur.
   if(2*index <= numTracker){
     if(nums[index] < nums[2*index]){
       leftTotal = nums[2*index] - nums[index];
-    
     }
   }
   if(2*index + 1 <= numTracker){
     if(nums[index] < nums[2*index + 1]){
       rightTotal = nums[2*index + 1] - nums[index];
-
     }
   }
+  //sorting and recalling the function.
   if(2*index <= numTracker){
-    if(leftTotal >= rightTotal){
+    if(leftTotal > rightTotal){
       int temp = nums[index];
       nums[index] = nums[2*index];
       nums[2*index] = temp;
@@ -105,38 +150,44 @@ void deleteSort(int (&nums)[101], int numTracker, int index){
       nums[index] = nums[2*index + 1];
       nums[2*index + 1] = temp;
       deleteSort(nums, numTracker, 2*index + 1);
+     
     }
   }
   return;
 }
-void add (int (&nums)[101], int &numTracker, int value){
+//this method adds a number to the end of the array and increments numTracker.
+void addMethod (int (&nums)[101], int &numTracker, int value){
   nums[numTracker + 1] = value;
-  cout << "NUMTRACKER: " << numTracker << "VALUE: " << value << endl;
   sort(nums, numTracker);
   numTracker++;
   return;
 }
 
+//this method swaps the first and last numbers and then removes the new last number, before calling the function that sorts the table.
 void remove(int (&nums)[101], int &numTracker){
-  cout << "NUMTRACKER " << numTracker << endl;
-  cout << "NUMTRACKER VALUE: " << nums[numTracker] << endl;
   int value = nums[numTracker];
   nums[1] = value;
   nums[numTracker] = -1;
   numTracker = numTracker - 1;
-  display(nums, numTracker);
   deleteSort(nums, numTracker, 1);
-  
-
-
-
 }
 
-void display(int nums [101], int numTracker){
-  for(int i = 1; i <= numTracker; i++){
-    cout << nums[i] << " " << endl;
-
+//this function prints the table.
+void display(int nums [101], int numTracker, int count, int index){
+  if((index*2) + 1 <= numTracker){
+    display(nums, numTracker, count + 1, (index*2) + 1);
+  }
+  //tabbing over the appropriate number based on the number of times recursion has occurred.
+  for(int i = 0; i < count; i++){
+    cout << '\t';
+  }
+  if(numTracker != 0){
+    cout << nums[index] << endl;
+  }
+  if((index * 2) <= numTracker){
+    display(nums, numTracker, count + 1, (index * 2));
   }
   
+    
 }
 
